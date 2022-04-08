7bfd95591ba06d68c0891a128e8cea2fa9a6dea9
ChrisDCP <Christopherdarielc@gmail.com>
2022-04-08 17:46:58 -0600

    ejercicio tipo pila terminado(no funciona del todo bien)

--- pila/pila/src/pila/Pila.java
+++ pila/pila/src/pila/Pila.java
@@ -15,71 +15,45 @@ public class Pila {
     /**
      * @param args the command line arguments
      */
-    public static int[] pila;
-    public static int max;
-    public static int tope =-1;
-    
+
+public static arreglo pi;
+  
     public static void main(String[] args) {
+
+      int MAX;
+      MAX = Integer.parseInt(JOptionPane.showInputDialog(null,"INGRESE EL TAMAÑO DE LA PILA"));  
+      pi= new arreglo(MAX);
+
+
        int opcion;
-       
-       JOptionPane.showInputDialog("Ingrese el tamaño del arreglo");
         
        do{
          opcion= Integer.parseInt(JOptionPane.showInputDialog("1- poner elemento en pila"+"\n"
-                                +"2- quitar dato de pila"+"\n"+"3- opcion 3"+"\n"
-                                +"4- opcion 4"+ "\n"+"5- mostrar datos de pila"+"\n"
+                                +"2- quitar dato de pila"+"\n"+"3- pila llena"+"\n"
+                                +"4- pila vacia"+ "\n"+"5- mostrar datos de pila"+"\n"
                                 +"6- salir"));
          
          switch(opcion){
-             case 1: llenarDato();
+             case 1: pi.llenarDato();
              break;
-         
+
+             case 2:pi.Quitardato();
+             break;
+
+             case 3: pi.pilallena();
+             break;
+
+             case 4: pi.pilavacia();
+             break;
+
+             case 5: pi.mostrarpila();
+             break;
+
+             default:JOptionPane.showMessageDialog(null,"Intente nuevamente","El numero no es una de las opciones"
+                                        ,JOptionPane.ERROR_MESSAGE);
          }
             
            
-       }while (opcion!=6);
-        
-    }
-    
-    public static void llenarDato(){
-        if(tope<max-1){
-        
-        pila[tope]= Integer.parseInt(JOptionPane.showInputDialog("ingresa el dato a poner en pila"));
-        tope++;
-        }
-        else if(tope ==max-1){JOptionPane.showMessageDialog(null,"pila llena");}
-            
-    }
-    
-    public static void QuitarDato(){
-    if (tope>-1){
-    pila[tope]=0;
-    tope--;
-        }else{JOptionPane.showMessageDialog(null,"pila vacia");}
-    }
-    
-    public static void funcion(){
-    
-    }
-    
-    public static void pilallena(){
-    
-    }
-    
-    public static void pilavacia(){
-    
-    }
-    
-    public static void mostrarpila(){
-        for (int i = max-1; i>0; i++) {
-            
-            if (pila[i]==0){
-            JOptionPane.showMessageDialog(null,"| |");
-            }else{
-            JOptionPane.showMessageDialog(null,(i+1)+"|"+pila[i]+"\n");
-            }
-            
-        }
-    }
-    
+       }while (opcion!=6);       
+    }    
 }
---
+++ pila/pila/src/pila/arreglo.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pila;
+
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author UNAN-Chontales
+ */
+public class arreglo {
+
+      int[] pila;
+      int max;
+      int tope;
+
+    public arreglo(int max) {
+        this.pila = new int [this.max+1];
+        this.max = max;
+        this.tope = -1;
+    }
+
+    public int[] getPila() {
+        return pila;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public int getTope() {
+        return tope;
+    }
+
+
+  public void llenarDato(){
+        if(this.tope<this.max-1){
+
+            int objeto= Integer.parseInt(JOptionPane.showInputDialog("ingresa el dato a poner en pila"));
+            
+            this.tope++;
+            this.pila[this.tope]= objeto;
+            
+
+            JOptionPane.showMessageDialog(null, "EL dato se inserto");
+        }
+        else {
+            JOptionPane.showMessageDialog(null,"pila llena");}
+        } 
+        
+  public void Quitardato(){
+    
+    int objeto = this.pila[this.tope];
+    tope--;
+    JOptionPane.showMessageDialog(null, "EL dato se elimino");
+    }
+    
+
+    public void pilallena(){
+    if (this.tope==this.max-1){
+        JOptionPane.showMessageDialog(null,"LA PILA ESTA LLENA",
+                                    "verificacion de pila llena",JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+    
+    public void pilavacia(){
+    if(this.tope == -1 ){
+        JOptionPane.showMessageDialog(null,"LA pila esta vacia",
+                                    "verificacion de pila vacia",JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+    
+    public void mostrarpila(){
+    if(this.tope == -1 ){
+        JOptionPane.showMessageDialog(null,"LA pila esta vacia");
+        }else {
+       String sumativa = "";
+            for (int i = -1; i <=this.tope; i++) {
+                sumativa += (i+1)*this.pila[i] +"\n"; 
+            }
+        JOptionPane.showMessageDialog(null, "LOS DATOS DE LA PILA SON : " + sumativa,
+                                "MOSTRAR DATOS",JOptionPane.INFORMATION_MESSAGE);
+
+        }
+        
+    }
+
+}
